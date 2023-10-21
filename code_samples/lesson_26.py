841330ed278b6a88f94ebb38aea7b4bdb237d708
V <v@yandex.ru>
2023-10-21 17:35:19 +0600

    lesson_26: Посмотрели на @property

--- code_samples/lesson_26.py
+++ code_samples/lesson_26.py
@@ -4,44 +4,62 @@ Lesson 26
 ООП - инкапсуляция
 """
 """
-Создание класса персоны, с приватными полями, а так же проверкой на валидность введенных данных в конструкторе и 
-сеттерах
+Декоратор @property - это специальный декоратор, 
+который позволяет нам обращаться к методу как к атрибуту.
+
+Нам не нужно будет писать круглые скобки после имени метода,
+когда мы будем обращаться к нему.
+
+Нам не нужно будет писать по 3 метода для получения, 
+изменения и удаления значения атрибута.
 """
 
 
 class Person:
-    def __init__(self, name: str, age: int):
-        self.__name = name
+    def __init__(self, name, surname, age):
+        self.name = name
+        self.surname = surname
         self.__age = age
 
-    def get_name(self):
-        return self.__name
-
-    def get_age(self):
+    @property  # Декоратор @property - это специальный декоратор, который позволяет нам обращаться к методу как к атрибуту.
+    def age(self):
+        print('Вызов метода age')
         return self.__age
 
-    def set_name(self, name):
-        if not isinstance(name, str):
-            raise ValueError('Имя должно быть строкой')
-        self.__name = name
+    @age.setter
+    def age(self, value):
+        print('Вызов метода age.setter')
+        if not self.__is_int(value):
+            raise ValueError('Возраст должен быть целым числом')
+        if not self.__is_positive(value):
+            raise ValueError('Возраст должен быть положительным числом')
+        self.__age = value
 
-    def set_age(self, age):
-        if not isinstance(age, int):
-            raise ValueError('Возраст должен быть числом')
-        if age < 0:
-            raise ValueError('Возраст не может быть отрицательным')
-        if age > 150:
-            raise ValueError('Он должен быть человеком а не вампиром!')
-        self.__age = age
+    @age.deleter
+    def age(self):
+        print('Вызов метода age.deleter')
+        del self.__age
+
+    def __is_int(self, value):
+        return isinstance(value, int)
+
+    def __is_positive(self, value):
+        return value > 0
+
+    def get_future_age(self, years:int):
+        """Метод который принимает число и возвращает возраст человека через X лет"""
+        return self.__age + years
 
     def __str__(self):
-        return (f'Имя: {self.__name}\n'
-                f'Возраст: {self.__age}\n')
+        return f'{self.name} {self.surname} {self.__age}'
 
 
-person = Person('John', 25)
-print(person)
-person.set_name('Ivan')
-# person.set_age(244)
-print(person)
+person = Person('John', 'Smith', 30)
+print(person.age)
+# person.age = 25
+print(person.get_future_age(5))
+# Удаляем атрибут age
+del person.age
+
+# print(person.age) # AttributeError: 'Person' object has no attribute '_Person__age'
 
