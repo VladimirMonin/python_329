56ea96b1192bfc4afd64727db58fd8bc23f98200
V <v@yandex.ru>
2023-11-18 16:51:21 +0600

    lesson_34.py: Декораторы. Пример 2х обвертываний с тегами HTML

--- code_samples/lesson_34.py
+++ code_samples/lesson_34.py
@@ -4,31 +4,11 @@ Lesson 34
 
 Тема: Декораторы классов и методов.
 - Вспоминаем что такое декораторы
+- Print vs return
+- Два декоратора на функцию
+- Пример с HTML тегами
 """
 from typing import Callable
 
 
-# Декоратор принт до принт после
 
-def print_decorator1(func: Callable) -> Callable:
-    def wrapper(*args, **kwargs):
-        if False:
-            return func(*args, **kwargs)
-
-    return wrapper
-
-
-def print_decorator2(func: Callable) -> Callable:
-    def wrapper(*args, **kwargs):
-        return func(*args, **kwargs)
-
-    return wrapper
-
-
-@print_decorator2
-@print_decorator1
-def some_print():
-    return ("Hello world")
-
-
-print(some_print())
