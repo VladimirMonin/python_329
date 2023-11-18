5f7499183f40053be87ae644ece98ca06609fafe
V <v@yandex.ru>
2023-11-18 17:58:59 +0600

    lesson_34.py: Декораторы. Функция для декорирования метода и функции. Логгер.

--- code_samples/lesson_34.py
+++ code_samples/lesson_34.py
@@ -7,46 +7,68 @@ Lesson 34
 - Print vs return
 - Два декоратора на функцию
 - Пример с HTML тегами
+
+Функция - декоратор класса
+- Добавляем новые поля
+- Добавляем новые методы
+
+Функция - декоратор методов
 """
-from typing import Callable
+from datetime import datetime
 
 
-# Декоратор класса. Добавим классу поле и метод
-def add_field_and_method(cls: type) -> type:
-    # self: type - указываем, что метод принимает экземпляр класса
-    # __class__ - возвращает класс экземпляра
-    # __name__ - возвращает имя класса
-    def some_method(self: type) -> None:
-        print(f'Вызван метод some_method у экземпляра класса {self.__class__.__name__}')
+# Функция для декорирования методов класса
+# Делает попытку вызова метода, если успешно, то возвращает результат, если нет, то логирует исклчение в файл
 
-    # Метод который принимает Фамилию и Имя. Создает новое поле full_name, ложит данные туда
-    # Делает сплит и кладет в поле first_name и last_name
-    def add_full_name(self: type, full_name: str) -> None:
-        self.full_name = full_name
-        self.first_name, self.last_name = full_name.split()
+def logger_decorator(func):
+    def wrapper(*args, **kwargs):
+        try:
+            result = func(*args, **kwargs)
+            return result
+        except Exception as ex:
+            # Определяем дату и время
+            current_datetime = datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+            # Определяем имя функции
+            function_name = func.__name__
+            # Определяем имя класса
+            class_name = args[0].__class__.__name__ # args[0] - это self
+            # Формируем лог строку
+            log_string = f'[{current_datetime}] В классе {class_name} в методе {function_name} произошла ошибка: {ex}\n'
+            # Записываем в файл
+            with open('logs.txt', 'a', encoding='utf-8') as file:
+                file.write(log_string)
 
-    cls.some_method = some_method
-    cls.add_full_name = add_full_name
-
-    return cls
-
-
-# Класс который мы будем декарировать и добавлять ему функционал и поля
-@add_field_and_method
-class SomeClass:
-    def __init__(self, name: str) -> None:
-        self.first_name = name
-
-    def get_name(self) -> str:
-        return self.name
+            return log_string
 
 
-# Создаем экземпляр класса и вызываем методы
-some_class = SomeClass(name='Иван')  # Создаем экземпляр класса
-some_class.some_method()  # Вызываем метод some_method (который был добавлен декоратором)
-some_class.add_full_name('Иван Иванов')  # Вызываем метод add_full_name (который был добавлен декоратором)
-print(some_class.full_name)  # Выводим поле full_name (которое было добавлено декоратором)
-print(some_class.first_name)  # Выводим поле first_name (которое было изначально)
-print(some_class.last_name)  # Выводим поле last_name (которое было добавлено декоратором)
+    return wrapper
 
 
+# Класс для декорирования (Math) с методом деления а на б
+
+class Math:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    @logger_decorator
+    def division(self):
+        return self.a / self.b
+
+
+# Создаем объект класса Math
+math = Math(10, 0)
+
+# Вызываем метод division
+print(math.division())
+
+# Пробуем декоратор на функции а не на методе класса. Функция деления
+
+
+@logger_decorator
+def get_division_func(a, b):
+    return a / b
+
+
+# Вызываем функцию
+print(get_division_func(10, 0))
