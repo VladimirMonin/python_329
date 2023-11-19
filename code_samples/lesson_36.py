32f197e037975a970f8ee1105873c0407cf91b0d
V <v@yandex.ru>
2023-11-19 18:23:04 +0600

    lesson_36.py: Практика. Решение

--- code_samples/lesson_36.py
+++ code_samples/lesson_36.py
@@ -15,3 +15,21 @@ Lesson 34
 Создайте классы A и B с этим метаклассом и проверьте, что у них есть
 атрибут class_id.
 """
+
+
+class IDClass(type):
+    def __new__(cls, name, bases, attrs):
+        attrs['class_id'] = name.lower()
+        return super().__new__(cls, name, bases, attrs)
+
+
+class A(metaclass=IDClass):
+    pass
+
+
+class B(metaclass=IDClass):
+    pass
+
+
+print(A.class_id)
+print(B.class_id)
