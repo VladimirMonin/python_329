b54a989ff6077e71bec1373f342fa76485b1fb34
V <v@yandex.ru>
2023-12-03 16:51:13 +0600

    lesson_42.py: init commit 03.12

--- code_samples/lesson_42.py
+++ code_samples/lesson_42.py
@@ -1,16 +1,16 @@
 """
-Lesson 40
-02.12.2023
+Lesson 42
+03.12.2023
 
-Тема: Разбор ДЗ по игре в города на ООП - HW_25.md
-- Добавление ручной сериализации и валидации данных + dataclass
+Тема: JSON Schema
+
 - JSON Schema - потребность в поиске более удобного способа валидации данных
 - Установка и использование библиотеки jsonschema
 
 pip install jsonschema
 """
 import jsonschema
-from jsonschema import validate
+from jsonschema import validate, ValidationError
 
 # "additionalProperties": false - запрещает добавлять в словарь новые ключи. Импорт
 # из jsonschema необходим для работы этого параметра
