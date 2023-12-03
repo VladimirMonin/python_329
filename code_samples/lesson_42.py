9c3ea6181e2b2f7d4d70d8cd5ef9ddf2125eddea
V <v@yandex.ru>
2023-12-03 16:59:38 +0600

    lesson_42.py: валидация marvel через validate и ValidationError

--- code_samples/lesson_42.py
+++ code_samples/lesson_42.py
@@ -6,6 +6,8 @@ Lesson 42
 
 - JSON Schema - потребность в поиске более удобного способа валидации данных
 - Установка и использование библиотеки jsonschema
+- validate
+- ValidationError
 
 pip install jsonschema
 """
@@ -14,6 +16,30 @@ from jsonschema import validate, ValidationError
 
 # "additionalProperties": false - запрещает добавлять в словарь новые ключи. Импорт
 # из jsonschema необходим для работы этого параметра
+
+from data.marvel import simple_set
+
+# Схема для валидации данных, которые импортируем из data.marvel
+# У нас массив, состоящий из строк, длинной от 3 до 100 символов
+schema = {
+    "type": "array",
+    "items": {
+        "type": "string",
+        "minLength": 3,
+        "maxLength": 100
+    }
+}
+
+# Валидация данных
+simple_list = list(simple_set)
+try:
+    validate(instance=simple_list, schema=schema)
+except ValidationError as error:
+    print(error)
+else:
+    print('Данные валидны')
+finally:
+    print('Валидация завершена')
 
 
 
