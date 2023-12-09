833404384a4735046468798295037050071be676
V <v@yandex.ru>
2023-12-09 16:35:47 +0600

    lesson_42.py: lesson_44: init

---
+++ code_samples/lesson_44.py
@@ -0,0 +1,13 @@
+"""
+Lesson 44
+03.12.2023
+
+Тема: Marshmallow
+- Установка pip install marshmallow
+- Импорт основных классов и инструментов
+
+"""
+
+from marshmallow import Schema, fields, ValidationError
+from dataclasses import dataclass
+from pprint import pprint
