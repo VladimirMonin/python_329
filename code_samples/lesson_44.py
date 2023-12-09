c8461b2820ff962cd4e902459ad9e1d24220ee71
V <v@yandex.ru>
2023-12-09 18:34:27 +0600

    lesson_42.py: lesson_44: практика!

--- code_samples/lesson_44.py
+++ code_samples/lesson_44.py
@@ -26,70 +26,11 @@ import json
 
 from data.marvel import full_dict
 
-marvel_films_json = """
-[
-    {
-        "title": "The Avengers",
-        "year": 2012
-    },
-    {
-        "title": "Avengers: Age of Ultron",
-        "year": 2015
-    },
-    {
-        "title": "Человек-паук: Вдали от дома",
-        "year": "2023"
-    }
-]
 """
+Практика десериализации данных!
 
-
-# Опишем датакласс, на основе которого будет создана схема для валидации данных
-
-@dataclass
-class Film:
-    title: str
-    year: int
-
-    def __str__(self):
-        return f"{self.title} ({self.year})"
-
-    def __repr__(self):
-        return f"{self.title} ({self.year})"
-
-
-# pip install marshmallow_dataclass
-# Создадим схему для валидации данных на основе датакласса Film
-
-FilmSchema = class_schema(Film)
-
-# Делаем валидацию и десериализацию данных - на выходе получаем объекты Film
-# Используем many=True, так как валидируем список словарей
-
-try:
-    films = FilmSchema(many=True).loads(marvel_films_json)
-
-
-except ValidationError as err:
-    print(err.messages)
-
-# Проверяем тип данных в списке - это объекты Film
-# [print(type(film)) for film in films]
-
-# Делаем обратное преобразование - сериализацию данных в JSON строку и выводим на печать
-films_list = FilmSchema(many=True)
-films_json = films_list.dumps(films)
-pprint(films_list)
-print(type(films_json))
-print(films_json)
-
-# Делаем из full_dict список словарей и сохраняем в JSON файл
-result = []
-
-for film in full_dict.values():
-    result.append(film)
-
-print(result)
-
-with open("../data/marvel.json", "w", encoding="utf-8") as file:
-    json.dump(result, file, ensure_ascii=False, indent=4)
+Создайте датакласс для валидации данных о фильме на основе json файла.
+Создайте схему на основе датакласса, используя библиотеку marshmallow_dataclass
+Создайте объект схемы и валидируйте данные из json файла (many=True)
+Получите на выходе список объектов датакласса
+"""
