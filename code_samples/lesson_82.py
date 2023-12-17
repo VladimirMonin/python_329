88703bd054c19c1896554c8caa034b0912fc55ba
V <v@yandex.ru>
2023-12-17 18:09:15 +0600

    lesson_82: Strategy

--- code_samples/lesson_82.py
+++ code_samples/lesson_82.py
@@ -6,79 +6,62 @@ Lesson 82
 - Паттерн "Абстрактная фабрика" - Abstract Factory
 - Паттерн "Строитель" - Builder
 - Паттерн "Фабричный метод" - Factory Method
+- Паттерн "Стратегия" - Strategy
 """
 
 from typing import List
 
 """
-Цель паттерна "Строитель" заключается в предоставлении способа конструирования сложного объекта пошагово. 
-Этот паттерн отделяет конструкцию сложного объекта от его представления, 
-так что один и тот же процесс конструирования может привести к созданию различных представлений.
+ Паттерн "Стратегия" – это один из фундаментальных шаблонов проектирования, 
+ который позволяет определить семейство алгоритмов, 
+ инкапсулировать каждый из них и сделать их взаимозаменяемыми. 
+ 
+ Этот паттерн позволяет изменять алгоритмы независимо от клиентов, которые их используют.
 """
 
 
-class Burger:
-    def __init__(self):
-        self.ingredients: List[str] = []
-
-    def add_ingredient(self, ingredient: str) -> None:
-        self.ingredients.append(ingredient)
+class Strategy:
+    def execute(self, data):
+        raise NotImplementedError
 
 
-class BurgerBuilder:
-    def __init__(self):
-        self.burger = Burger()
-
-    def add_lettuce(self) -> None:
-        self.burger.add_ingredient("салат")
-
-    def add_tomato(self) -> None:
-        self.burger.add_ingredient("помидор")
-
-    def add_cheese(self) -> None:
-        self.burger.add_ingredient("сыр")
-
-    def get_burger(self) -> Burger:
-        return self.burger
+class ConcreteStrategyA(Strategy):
+    def execute(self, data):
+        return sum(data)
 
 
-# Клиентский код
-builder = BurgerBuilder()
-builder.add_cheese()
-builder.add_lettuce()
-burger = builder.get_burger()
-print(burger.ingredients)
+class ConcreteStrategyB(Strategy):
+    def execute(self, data):
+        return max(data)
+
+class ConcreteStrategyС(Strategy):
+    def execute(self, data):
+        return min(data)
 
 
-class Document:
-    def __init__(self):
-        self.pages: List[str] = []
+class Context:
+    def __init__(self, strategy: Strategy):
+        self._strategy = strategy
 
-    def add_page(self, page: str) -> None:
-        self.pages.append(page)
+    def set_strategy(self, strategy: Strategy):
+        self._strategy = strategy
 
-
-class DocumentBuilder:
-    def __init__(self):
-        self.document = Document()
-
-    def add_title(self, title: str) -> None:
-        self.document.add_page(f"Заголовок: {title}")
-
-    def add_paragraph(self, paragraph: str) -> None:
-        self.document.add_page(f"Абзац: {paragraph}")
-
-    def add_footer(self, footer: str) -> None:
-        self.document.add_page(f"Подвал: {footer}")
-
-    def get_document(self) -> Document:
-        return self.document
+    def execute_strategy(self, data):
+        return self._strategy.execute(data)
 
 
-# Клиентский код
-doc_builder = DocumentBuilder()
-doc_builder.add_title("Документация на Python")
-doc_builder.add_paragraph("Python - мощный язык программирования.")
-doc_builder.add_footer("Конец документа")
-document = doc_builder.get_document()
-print("\n".join(document.pages))
+# Пример использования
+data = [1, 2, 3, 4, 5]
+context = Context(ConcreteStrategyA())
+result = context.execute_strategy(data)
+print("Результат ConcreteStrategyA:", result)
+
+context.set_strategy(ConcreteStrategyB())
+result = context.execute_strategy(data)
+print("Результат ConcreteStrategyB:", result)
+
+context.set_strategy(ConcreteStrategyС())
+result = context.execute_strategy(data)
+print("Результат ConcreteStrategyС:", result)
+
+
