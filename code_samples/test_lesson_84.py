1195d02b67486cd1efdef94519c71ec6e6de70b1
V <v@yandex.ru>
2023-12-23 18:42:10 +0600

    lesson_84: parametrize

--- code_samples/test_lesson_84.py
+++ code_samples/test_lesson_84.py
@@ -1,4 +1,5 @@
 import pytest
+
 from lesson_84 import multiply
 
 
@@ -24,3 +25,51 @@ def test_negative_multiply():
     assert multiply(2, 3) == 7
 
 
+# Параметризация тестов - это когда мы передаем
+# в тест несколько значений
+
+# @pytest.mark.parametrize("a, b, expected", [
+#     (2, 3, 6),
+#     (3, 4, 12),
+#     (4, 5, 20),
+#     (5, 5, 25),
+#     (2, 2, 4),
+# ])
+# def test_multiply(a, b, expected):
+#     """
+#     Параметризованный тест для функции multiply
+#     """
+#     assert multiply(a, b) == expected, f'Должно быть {expected}'
+#
+
+data_set = [
+    (2, 3, 6),
+    (3, 4, 12),
+    (4, 5, 20),
+    (5, 5, 25),
+    (2, 2, 4),
+]
+
+
+@pytest.mark.parametrize("a, b, expected", data_set)
+def test_multiply(a, b, expected):
+    """
+    Параметризованный тест для функции multiply
+    """
+    assert multiply(a, b) == expected, f'Должно быть {expected}'
+
+
+# Параметризация xfail
+
+@pytest.mark.parametrize("a, b, expected", [
+    (2, 3, 6),
+    (3, 4, 12),
+    (4, 5, 20),
+    pytest.param(5, 5, 26, marks=pytest.mark.xfail), # помечаем тест, который всегда падает
+    (2, 2, 4),
+])
+def test_multiply(a, b, expected):
+    """
+    Параметризованный тест для функции multiply
+    """
+    assert multiply(a, b) == expected, f'Должно быть {expected}'
