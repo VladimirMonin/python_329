0d80159cec71c72f07e52b62ddc3f81172c2ff38
V <v@yandex.ru>
2023-12-23 17:13:08 +0600

    lesson_84: assert

--- code_samples/lesson_84.py
+++ code_samples/lesson_84.py
@@ -6,3 +6,43 @@ Lesson 84: Pytest
 - Установка pytest (pip install pytest)
 - assert
 """
+
+# assert - это проверка условия,
+# утверждение, которое должно быть истинным,
+# иначе будет ошибка.
+
+# Пример 2+2=4
+assert 2 + 2 == 4, "Ожидалось 4"
+
+
+# assert 2 + 2 == 5, "Ожидалось 4"
+
+def sum(a, b):
+    return a + b
+
+
+# assert sum(2, 2) == 4, "Ожидалось 4"
+
+# Скрипт с услоивем if и assert
+if __name__ == "__main__":
+    try:
+        assert sum(2, 2) == 4, f"Ожидалось 4 получили {sum(2, 2)}"
+    except AssertionError as e:
+        print(e)
+
+    try:
+        assert sum(3, 3) == 6, f"Ожидалось 6 получили {sum(3, 3)}"
+    except AssertionError as e:
+        print(e)
+
+# Негативный тест с ожиданием исключения
+
+# Функция деления
+def divide(a, b):
+    return a / b
+
+# Тест деления на 0
+try:
+    divide(2, 0)
+except ZeroDivisionError as e:
+    print(e)
