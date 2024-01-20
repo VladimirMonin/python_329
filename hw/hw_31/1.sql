44a25a061485e7e5df84c1a2f70c0e8803a6b19d
V <v@yandex.ru>
2024-01-20 11:02:49 +0600

    lesson_46: hw_31 решение

--- hw/hw_31/1.sql
+++
@@ -1,4 +0,0 @@
-SELECT id, name, city, address, category, subcategory
-FROM [2gis_businesses]
-WHERE city = "Балашиха" AND category IN ("Продажа билеов", "Авиатранспорт")
-ORDER BY subcategory, name DESC
