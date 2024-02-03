a90f112789587205e6a6f33f887ce2079236324d
v <v@mail.ru>
2024-02-03 18:34:13 +0600

    lesson_51: первая табилца в Алхимии

--- code_samples/lesson_51.py
+++ code_samples/lesson_51.py
@@ -20,6 +20,8 @@ Lesson 51 - Знакомство с SQLAlchemy 2.0
 
 import os
 from dotenv import load_dotenv
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Загрузка переменных окружения из файла .env
 load_dotenv()
@@ -30,3 +32,46 @@ print(DATABASE_URL)
 # sqlite:///root/data/base.db
 # postgresql://user:password@localhost:5432/db_name
 # mysql://user:password@localhost:3306/db_name
+
+"""
+Почему важно хранить данные в переменных окружения?
+- Безопасность
+- Переносимость
+- Удобство использования
+- Отладка
+
+"""
+
+# create_engine - создание объекта Engine
+# - echo - выводит все SQL-запросы, которые отправляются в базу данных
+# - future - поддержка новых возможностей SQLAlchemy 2.0
+# DeclareBase - базовый класс для создания моделей
+# sessionmaker - создание объекта Session для работы с базой данных
+# - autocommit - автоматическое подтверждение транзакций
+# - autoflush - автоматическая очистка сессии
+# - bind - связь с объектом Engine
+
+
+# Создание объекта Engine
+engine = create_engine(DATABASE_URL, echo=True, future=True)
+
+# Определение базового класса для создания моделей
+Base = declarative_base()
+
+# Создание объекта Session
+Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# Пример модели
+class User(Base):
+    """
+    Модель пользователя
+    """
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    age = Column(Integer)
+
+
+# Создание таблиц в базе данных
+Base.metadata.create_all(bind=engine)
